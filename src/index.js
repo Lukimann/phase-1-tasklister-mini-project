c80a26790ad3e16d83b24296da47dfb64f3d237d
Luka Wanyama <lukewanyama5@gmail.com>
2024-01-03 12:36:42 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form");
+  
+  let tasksList = document.querySelector("#tasks");
+  //above i created two variable to house the ids tasks and form.
+  
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let taskDescription = document.querySelector("#new-task-description").value;
+
+    if (taskDescription !== "") {
+      tasksList.innerHTML += `<li>${taskDescription}</li>`;
+      document.querySelector("#new-task-description").value = "";
+    }
+  });
 });
